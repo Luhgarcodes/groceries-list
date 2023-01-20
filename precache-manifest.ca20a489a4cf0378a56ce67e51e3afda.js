9996417a280b781dd7cee04548c6aec52b844935
luhgar <juniorjumper46@.com>
2023-01-20 10:55:24 +0530

    Updates

---
+++ precache-manifest.ca20a489a4cf0378a56ce67e51e3afda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9532c884c0a3b237c122",
+    "url": "/groceries-list/static/css/main.13156218.chunk.css"
+  },
+  {
+    "revision": "9532c884c0a3b237c122",
+    "url": "/groceries-list/static/js/main.9532c884.chunk.js"
+  },
+  {
+    "revision": "4f15f65dd70481093383",
+    "url": "/groceries-list/static/js/1.4f15f65d.chunk.js"
+  },
+  {
+    "revision": "1d5bb76e1426a5664b61",
+    "url": "/groceries-list/static/js/runtime~main.1d5bb76e.js"
+  },
+  {
+    "revision": "920ef1c738b4e03ec0ad661bb117d484",
+    "url": "/groceries-list/static/media/deletebtn.920ef1c7.png"
+  },
+  {
+    "revision": "4df3793dd8af7b3cf331c4f322a6de87",
+    "url": "/groceries-list/index.html"
+  }
+];
